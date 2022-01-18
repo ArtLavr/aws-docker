c2d7a4e004ed4ce012ecd1e1e223a6c8fb57781b
ArtLavr <onemurder39@gmail.com>
2022-01-18 22:04:59 +0300

    init project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const PORT = process.env.PORT || 8000
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.get("/status", (req, res) => res.send({ status: "༼ つ ◕_◕ ༽つ" }))
+
+app.listen(PORT, () =>
+  console.log(`server started on port ${PORT} ༼ つ ◕_◕ ༽つ`)
+)
